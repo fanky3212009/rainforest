44380225783a00384b14323a11b4fe867946345f
Franky Chau <houun.chau@gmail.com>
2016-01-28 17:42:23 -0500

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   # 7 ReSTful routes
   # get '/products' => 'products#index'
