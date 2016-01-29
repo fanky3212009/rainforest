b4f18a40868c02da88e92d9e602f69cabfe7b066
Franky Chau <houun.chau@gmail.com>
2016-01-29 16:08:50 -0500

    Add review model and associations between user, product and review models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :products, through: :reviews
   # ^ automatically add attr_accessor :password, :password_confirmation
   # which sets up virtual attributes (not database-backed) with those names so we can easily create a form that accepts those values from the user
   # and has_secure_password will take those values and encrypt them into the password_digest field on the users table.
