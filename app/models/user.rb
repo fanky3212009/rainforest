1f4de0c2d20d7572112a0a3a1d9659beee5ef12d
Franky Chau <houun.chau@gmail.com>
2016-01-29 17:32:00 -0500

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :products, through: :reviews
+  validates :name, presence: true
   # ^ automatically add attr_accessor :password, :password_confirmation
   # which sets up virtual attributes (not database-backed) with those names so we can easily create a form that accepts those values from the user
   # and has_secure_password will take those values and encrypt them into the password_digest field on the users table.
